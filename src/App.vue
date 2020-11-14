07e9fe3f74b75c58017fb203d4ab0035452931f7
h6k2 <sepak.hirokazu10@gmail.com>
2020-11-14 13:13:45 +0900

    コンポーネント追加

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,54 @@
 <template>
-  <div>
-    <p>
-      サンプル
-    </p>
+  <div class="container">
+    <myheader></myheader>
+    <mainkv></mainkv>   
+        
   </div>
 </template>
+
+<script>
+import myheader from './components/myheader'
+import mainkv from './components/mainkv'
+
+export default {
+  components: {
+    myheader,
+    mainkv
+  }
+}
+</script>
+
+<style>
+html, body, div, span, applet, object, iframe,
+h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+a, abbr, acronym, address, big, cite, code,
+del, dfn, em, img, ins, kbd, q, s, samp,
+small, strike, strong, sub, sup, tt, var,
+b, u, i, center,
+dl, dt, dd, ol, ul, li,
+fieldset, form, label, legend,
+table, caption, tbody, tfoot, thead, tr, th, td,
+article, aside, canvas, details, embed,
+figure, figcaption, footer, header, hgroup,
+menu, nav, output, ruby, section, summary,
+time, mark, audio, video {
+  margin: 0;
+  padding: 0;
+  border: 0;
+  font-size: 100%;
+  font: inherit;
+  vertical-align: baseline;
+}
+.container {
+  height: 100vh;
+  background: #02001c;
+  background: -webkit-gradient(linear, left top, right top, color-stop(21%, #001b27), color-stop(50%, #003001), color-stop(79%, #2b0000), to(#160000));
+  background: linear-gradient(90deg, #001b27 21%, #003001 50%, #2b0000 79%, #160000 100%);
+}
+
+.abl {
+  position: absolute;
+}
+
+
+</style>
