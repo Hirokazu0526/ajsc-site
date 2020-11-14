60e8dda823467c1ef9fad39bd294c1819f0b5833
h6k2 <sepak.hirokazu10@gmail.com>
2020-11-14 13:14:25 +0900

    コンポーネント追加

---
+++ src/components/mainkv.vue
@@ -0,0 +1,86 @@
+<template>
+    <div class="main_kv">
+        <img src="/img/shunichi.png" alt="" class="main_kv_01 abl fadeIn_1">
+        <img src="/img/hirokazu.png" alt="" class="main_kv_02 abl fadeIn_2">
+        <img src="/img/toshitaka.png" alt="" class="main_kv_03 abl fadeIn_3">
+        <img src="/img/masuda.png" alt="" class="main_kv_04 abl fadeIn_4">
+        <img src="/img/takano.png" alt="" class="main_kv_05 abl fadeIn_5">
+        <img src="/img/okamoto.png" alt="" class="main_kv_06 abl fadeIn_6">
+        <img src="/img/sato.png" alt="" class="main_kv_07 abl fadeIn_7">
+        <img src="/img/yumi.png" alt="" class="main_kv_08 abl fadeIn_8">
+        <img src="/img/naoko.png" alt="" class="main_kv_09 abl fadeIn_9">
+        <h3 class="main_text 2019S"></h3>
+        <h3 class="main_text 2019W"></h3>
+        <h3 class="main_text 2020S"></h3>
+    </div>
+</template>
+<style scoped>
+    .main_kv {
+  position: relative;
+  width: 100vw;
+  height: 714px;
+}
+
+.main_kv_01 {
+  top: 200px;
+  left: -150px;
+  width: 40%;
+  z-index: 5;
+}
+
+.main_kv_02 {
+  top: 230px;
+  left: 120px;
+  width: 38%;
+  z-index: 5;
+}
+
+.main_kv_03 {
+  top: 40px;
+  left: -150px;
+  width: 40%;
+  z-index: 4;
+}
+
+.main_kv_04 {
+  top: 0px;
+  right: 10px;
+  width: 70%;
+  z-index: 5;
+}
+
+.main_kv_05 {
+  top: -100px;
+  right: 20px;
+  width: 75%;
+  z-index: 5;
+}
+
+.main_kv_06 {
+  top: -130px;
+  right: -180px;
+  width: 60%;
+  z-index: 3;
+}
+
+.main_kv_07 {
+  top: 20px;
+  left: 480px;
+  width: 35%;
+  z-index: 5;
+}
+
+.main_kv_08 {
+  top: -37px;
+  left: 160px;
+  width: 45%;
+  z-index: 5;
+}
+
+.main_kv_09 {
+  top: 20px;
+  left: 250px;
+  width: 60%;
+  z-index: 20;
+}
+</style>
